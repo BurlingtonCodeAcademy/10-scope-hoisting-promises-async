baa7a9ecf3157f8a857d9b16483cad10f21267c3
Chris Boydstun <52934303+cboydstun@users.noreply.github.com>
2024-02-21 17:45:48 -0600

    promise async function example

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('BIG LESSON TODAY!')
+function delayedMessage(message, delay) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(message);
+        }, delay);
+    });
+}
+
+// async function with await
+async function printMessage() {
+    const messageOne = await delayedMessage("Message One", 4000);
+    console.log(messageOne);
+
+    const messageThree = await delayedMessage("Message Three", 1000);
+    console.log(messageThree);
+};
+
+async function printAnotherMessage() {
+    const messageTwo = await delayedMessage("Message Two", 2000);
+    console.log(messageTwo);
+}
+
+printAnotherMessage();
+printMessage();
